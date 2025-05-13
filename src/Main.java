30fcd1bbfaa31edb0e50274be61b2009f6d77522
Prue0115 <prue0115@gmail.com>
2025-05-13 11:21:34 +0900

    N까지의 홀수의 합을 계산하는 프로그램

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+        public class Main {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        System.out.print("숫자를 입력하세요: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+        if (i % 2 == 1) {
+        sum += i;
+        }
+        }
+
+        System.out.println("홀수의 합: " + sum);
+        }
+        }
